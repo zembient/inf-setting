7ef0f39b191d651c342d845bb92fc6c1240601c4
Zen Iwasaki <gravityblast515@gmail.com>
2018-10-02 22:15:44 +0900

    delete unnecessary things

--- liff-starter.js
+++ liff-starter.js
@@ -1,64 +1,22 @@
-const timing = {
-  1: "食前",
-  2: "食後",
-  3: "食間"
-};
-
-const meal = {
-  1: "朝食",
-  2: "昼食",
-  3: "夕食"
-};
-
 window.onload = function(e) {
   liff.init(
     data => {
       document.getElementById("register").addEventListener("click", function() {
-        const medicine = document.getElementById("medicine").value;
+        const childrenName = document.getElementById("childrenName").value;
+        const grandparentsName = document.getElementById("grandparentsName").value;
 
-        let timingList = [];
-        let mealList = [];
-
-        const syokuzen = document.getElementById("syokuzen");
-        if (syokuzen.checked) {
-          timingList.push(syokuzen.value);
-        }
-        const syokugo = document.getElementById("syokugo");
-        if (syokugo.checked) {
-          timingList.push(syokugo.value);
-        }
-        const syokkan = document.getElementById("syokkan");
-        if (syokkan.checked) {
-          timingList.push(syokkan.value);
-        }
-
-        const asa = document.getElementById("asa");
-        if (asa.checked) {
-          mealList.push(asa.value);
-        }
-        const hiru = document.getElementById("hiru");
-        if (hiru.checked) {
-          mealList.push(hiru.value);
-        }
-        const yoru = document.getElementById("yoru");
-        if (yoru.checked) {
-          mealList.push(yoru.value);
-        }
-
-        console.log(medicine);
-        console.log(timingList);
-        console.log(mealList);
-
-        for (num of timingList) {
-        }
-        console.log();
+        console.log(childrenName);
+        console.log(parentsName);
 
         console.log("register button clicked!");
+
+        
+
         liff
           .sendMessages([
             {
               type: "text",
-              text: `<お薬リマインダーを登録する>\n【お薬名　　】：${medicine}\n【タイミング】：${timing}`
+              text: `<登録したもん！>\n 子供の名前：${childrenName}\ 祖父母の名前：${parentsName}`
             },
             {
               type: "text",
